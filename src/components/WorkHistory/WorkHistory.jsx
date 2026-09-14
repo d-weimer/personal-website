d2bc59bda907f62c48d799ed5c5432615722bfc5
Daniel Weimer <dweimer19@gmail.com>
2026-09-14 16:02:10 +0900

    Initial commit and years added

--- src/components/WorkHistory/WorkHistory.jsx
+++ src/components/WorkHistory/WorkHistory.jsx
@@ -1,7 +1,25 @@
 import "./WorkHistory.css";
 
 function WorkHistory() {
-  return <section>Timeline</section>;
+  return (
+    <section className="timeline">
+      <div className="timeline__content">
+        <p>2014</p>
+        <p>2015</p>
+        <p>2016</p>
+        <p>2017</p>
+        <p>2018</p>
+        <p>2019</p>
+        <p>2020</p>
+        <p>2021</p>
+        <p>2022</p>
+        <p>2023</p>
+        <p>2024</p>
+        <p>2025</p>
+        <p>2026</p>
+      </div>
+    </section>
+  );
 }
 
 export default WorkHistory;
