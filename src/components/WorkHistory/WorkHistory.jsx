86f29a58c04e64df8a9073af5dd9937fb5f333dd
Daniel Weimer <dweimer19@gmail.com>
2026-09-16 02:16:16 +0900

    Updated and added timeline to WorkHistory

--- src/components/WorkHistory/WorkHistory.jsx
+++ src/components/WorkHistory/WorkHistory.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 import "./WorkHistory.css";
 import { WORK_HISTORY_DATA } from "../../utils/workHistoryData.js";
+import timeline from "../../assets/timeline.svg";
 
 function WorkHistory() {
   const { year } = useParams();
@@ -13,21 +14,28 @@ function WorkHistory() {
 
   return (
     <section className="work-history">
+      <div className="work-history__timeline">
+        <img src={timeline} alt="Timeline" />
+      </div>
+
       <div className="work-history__content">
         {activeYearData &&
           activeYearData.event.map((event, index) => (
             <div key={index} className="work-history__card">
-              {/*
-              <span className={`bullet bullet--${event.type}`}>
-                {event.type}
+              <span
+                className={`work-history__bullet work-history__bullet--${event.type}`}
+              >
+                &bull;
               </span>
-              */}
-
-              {event.role && <p className="work-history__role">{event.role}</p>}
-              <p className="work-history__description">{event.description}</p>
-              {event.project && (
-                <p className="work-history__project">{event.project}</p>
-              )}
+              <div className="work-history__card-content">
+                {event.role && (
+                  <p className="work-history__role">{event.role}</p>
+                )}
+                <p className="work-history__description">{event.description}</p>
+                {event.project && (
+                  <p className="work-history__project">{event.project}</p>
+                )}
+              </div>
             </div>
           ))}
       </div>
