d9d570b38a28132b8a2299a9f8042bace15c55c1
Daniel Weimer <dweimer19@gmail.com>
2026-09-17 06:35:25 +0900

    Setup software project grid and project card styling

--- src/components/SEProjectCard/SEProjectCard.jsx
+++ src/components/SEProjectCard/SEProjectCard.jsx
@@ -1,7 +1,25 @@
 import "./SEProjectCard.css";
 
-function SEProjectCard() {
-  return <section className="software">Software Card</section>;
+function SEProjectCard({ project, onCardClick }) {
+  return (
+    <div className="software-card" onClick={onCardClick}>
+      <div className="software-card__container">
+        <div className="software-card__thumbnail-container">
+          <img
+            src={project.thumbnail}
+            alt={project.title}
+            className="software-card__thumbnail"
+          />
+        </div>
+        <div className="software-card__content">
+          <h3 className="software-card__title">{project.title}</h3>
+          <p className="software-card__description">
+            {project.shortDescription}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SEProjectCard;
--- src/components/SEProjects/SEProjects.jsx
+++ src/components/SEProjects/SEProjects.jsx
@@ -1,12 +1,31 @@
+import React, { useState } from "react";
+
 import "./SEProjects.css";
 import SEProjectCard from "../SEProjectCard/SEProjectCard";
 import SEProjectPanel from "../SEProjectPanel/SEProjectPanel";
+import { SOFTWARE_PROJECT_DATA } from "../../utils/softwareProjectData.js";
 
 function SEProjects() {
+  const [selectedProject, setSelectedProject] = useState(null);
+
   return (
     <section className="software">
-      <SEProjectCard />
-      <SEProjectPanel />
+      <div className="software__grid">
+        {SOFTWARE_PROJECT_DATA.map((project) => (
+          <SEProjectCard
+            key={project.projectId}
+            project={project}
+            onCardClick={() => setSelectedProject(project)}
+          />
+        ))}
+      </div>
+
+      {selectedProject && (
+        <SEProjectPanel
+          project={selectedProject}
+          onClose={() => setSelectedProject(null)}
+        />
+      )}
     </section>
   );
 }
