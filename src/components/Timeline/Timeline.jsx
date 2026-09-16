b8341d34642dbf20ba480c165ed916c760fbd383
Daniel Weimer <dweimer19@gmail.com>
2026-09-16 15:07:50 +0900

    Added visual indication when selecting a year

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -2,49 +2,37 @@ import { NavLink } from "react-router-dom";
 
 import "./Timeline.css";
 
+const years = [
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+  "2021",
+  "2022",
+  "2023",
+  "2024",
+  "2025",
+  "2026",
+];
+
 function Timeline() {
   return (
     <section className="timeline">
       <div className="timeline__content">
-        <NavLink to="/work-history/2014" className="timeline__link">
-          2014
-        </NavLink>
-        <NavLink to="/work-history/2015" className="timeline__link">
-          2015
-        </NavLink>
-        <NavLink to="/work-history/2016" className="timeline__link">
-          2016
-        </NavLink>
-        <NavLink to="/work-history/2017" className="timeline__link">
-          2017
-        </NavLink>
-        <NavLink to="/work-history/2018" className="timeline__link">
-          2018
-        </NavLink>
-        <NavLink to="/work-history/2019" className="timeline__link">
-          2019
-        </NavLink>
-        <NavLink to="/work-history/2020" className="timeline__link">
-          2020
-        </NavLink>
-        <NavLink to="/work-history/2021" className="timeline__link">
-          2021
-        </NavLink>
-        <NavLink to="/work-history/2022" className="timeline__link">
-          2022
-        </NavLink>
-        <NavLink to="/work-history/2023" className="timeline__link">
-          2023
-        </NavLink>
-        <NavLink to="/work-history/2024" className="timeline__link">
-          2024
-        </NavLink>
-        <NavLink to="/work-history/2025" className="timeline__link">
-          2025
-        </NavLink>
-        <NavLink to="/work-history/2026" className="timeline__link">
-          2026
-        </NavLink>
+        {years.map((year) => (
+          <NavLink
+            key={year}
+            to={`/work-history/${year}`}
+            className={({ isActive }) =>
+              `timeline__link ${isActive ? "timeline__link--active" : ""}`
+            }
+          >
+            {year}
+          </NavLink>
+        ))}
       </div>
     </section>
   );
