0bc123670a915825783e6fb3c593bf8eb9602e97
Daniel Weimer <dweimer19@gmail.com>
2026-09-18 16:43:00 +0900

    Implemented software project panel base, updated card styling

--- src/components/SEProjectPanel/SEProjectPanel.jsx
+++ src/components/SEProjectPanel/SEProjectPanel.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 import "./SEProjectPanel.css";
+import panelClose from "../../assets/close-button.svg";
 
 function SEProjectPanel({ project, onClose }) {
   useEffect(() => {
@@ -13,7 +14,30 @@ function SEProjectPanel({ project, onClose }) {
 
   if (!project) return null;
 
-  return <section className="software">Software Panel</section>;
+  return (
+    <section className="software-panel" onClick={onClose}>
+      <div
+        className="software-panel__content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={onClose}
+          type="button"
+          className="software-panel__close"
+        >
+          <img
+            src={panelClose}
+            alt="Close Button"
+            className="software-panel__close-button"
+          />
+        </button>
+        <h3 className="software-panel__title">{project.title}</h3>
+        <p className="software-panel__description">
+          {project.longDescription || project.shortDescription}
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default SEProjectPanel;
