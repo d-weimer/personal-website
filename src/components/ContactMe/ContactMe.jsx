ee52205eb4c5c7225cdcaf37193fee57181ff0d7
Daniel Weimer <dweimer19@gmail.com>
2026-09-22 15:30:37 +0900

    Implemented layout for contact me form

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,7 +1,23 @@
 import "./ContactMe.css";
 
 function ContactMe() {
-  return <section className="contact">Contact Me</section>;
+  return (
+    <section className="contact">
+      <div className="contact__name">
+        <p className="contact__label">Name</p>
+        <input className="contact__input" type="text" />
+      </div>
+      <div className="contact__email">
+        <p className="contact__label">Email</p>
+        <input className="contact__input" type="text" />
+      </div>
+      <div className="contact__message">
+        <p className="contact__label">Message</p>
+        <input className="contact__input-message" type="text" />
+      </div>
+      <button className="contact__button">Send</button>
+    </section>
+  );
 }
 
 export default ContactMe;
