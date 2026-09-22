afff63baa935b8eb656194fc464f69b02ec71325
Daniel Weimer <dweimer19@gmail.com>
2026-09-22 15:49:45 +0900

    Updated contact me form styling and implemented Message restraints

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -5,17 +5,37 @@ function ContactMe() {
     <section className="contact">
       <div className="contact__name">
         <p className="contact__label">Name</p>
-        <input className="contact__input" type="text" />
+        <input
+          className="contact__input"
+          id="form-name"
+          type="text"
+          placeholder="Name"
+          required
+        />
       </div>
       <div className="contact__email">
         <p className="contact__label">Email</p>
-        <input className="contact__input" type="text" />
+        <input
+          className="contact__input"
+          id="form-email"
+          type="email"
+          placeholder="Email"
+          required
+        />
       </div>
       <div className="contact__message">
         <p className="contact__label">Message</p>
-        <input className="contact__input-message" type="text" />
+        <textarea
+          className="contact__input-message"
+          id="form-message"
+          placeholder="Message..."
+          rows="6"
+          required
+        />
       </div>
-      <button className="contact__button">Send</button>
+      <div className="contact__button-container">
+        <button className="contact__button">Send</button>
+      </div>
     </section>
   );
 }
